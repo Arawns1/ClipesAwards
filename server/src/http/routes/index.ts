e6f29b8c0b14ef7463c5296b8671b59ec1794983
Gabriel Damico <gabrieldamico22@gmail.com>
2025-04-14 22:54:34 -0300

    feat: adiçao de banco de dados e rota status

--- server/src/http/routes/index.ts
+++ server/src/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { getAllClips } from "./get-all-clips";
 import { ping } from "./ping";
+import { status } from "./status";
 
 export default async function routes(app: FastifyInstance) {
   app.register(ping);
   app.register(getAllClips);
+  app.register(status);
 }
---
+++ server/src/http/routes/status.ts
@@ -0,0 +1,57 @@
+import { FastifyInstance } from "fastify";
+import database from "infra/database";
+import pool from "infra/database";
+
+export async function status(app: FastifyInstance) {
+  app.get("/status", async (request, response) => {
+    console.group("[status]");
+    console.log(`[INFO] Requisição recebida`);
+    try {
+      const updatedAt = new Date().toISOString();
+      const dbVersionResult = await database.query({
+        text: "SHOW server_version;",
+      });
+      const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+      const dbMaxConnectionsResult = await database.query({
+        text: "SHOW max_connections;",
+      });
+      const dbMaxConnectionsValue =
+        dbMaxConnectionsResult.rows[0].max_connections;
+
+      const dbName = process.env.POSTGRES_DB;
+      const dbOpenConnectionsResult = await database.query({
+        text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+        values: [dbName],
+      });
+      const dbOpenConnectionsValue = dbOpenConnectionsResult.rows[0].count;
+      const poolMaxConnections = database.pool.options.max;
+      const poolIdleClients = database.pool.idleCount;
+      const poolTotalClients = database.pool.totalCount;
+
+      const responseBody = {
+        updated_at: updatedAt,
+        dependencies: {
+          database: {
+            max_connections: parseInt(dbMaxConnectionsValue),
+            opened_connections: dbOpenConnectionsValue,
+            version: dbVersionValue,
+            pool_max_connections: poolMaxConnections,
+            pool_idle_clients: poolIdleClients, // conexões ociosas
+            pool_connected_clients: poolTotalClients, // total de clientes conectados
+          },
+        },
+      };
+
+      return response.status(200).send(responseBody);
+    } catch (err) {
+      console.error("[ERROR] Erro interno: ", err);
+      return response
+        .code(500)
+        .send({ error: "Erro interno. Tente novamente mais tarde" });
+    } finally {
+      console.info(`[INFO] Requisição finalizada`);
+      console.groupEnd();
+    }
+  });
+}
